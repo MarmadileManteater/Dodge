6d9908942f329c4677e35b787eddfb089383b671
Emma <MarmadileManteater@proton.me>
2025-06-09 17:27:51 -0400

    Use line to spawn projectiles all around screen

--- scripts/number_1.gd
+++ scripts/number_1.gd
@@ -1,26 +1,18 @@
 extends Node2D
 
 var player: RigidBody2D
+var outline: Line2D
 var projectiles: Array
 var timer: Timer
 var counter = 0
 
 func generate_coord_around_viewport():
-	var viewport_size = get_viewport().size
-	var viewport_x = viewport_size[0]
-	var viewport_y = viewport_size[1]
-	var x = (randi() % (viewport_x + 200) ) - 100
-	var y = 0
-	if (x < 100 || x > viewport_x):
-		print("on edge")
-		#y = (randi() % (viewport_y + 200)) - 100
-	return Vector2(x, y)
+	return outline.get_point_position(randi() % outline.get_point_count())
+
 func spawn_projectile(projectile: RigidBody2D):
 	var new_projectile = projectile.duplicate()
-	# todo set new position
 	var position = generate_coord_around_viewport()
 	new_projectile.position = position
-	print(position.direction_to(player.position).angle())
 	new_projectile.rotation = position.direction_to(player.position).angle() - 1
 	new_projectile.linear_velocity = position.direction_to(player.position) * 250
 	new_projectile.name = "%d-%s" % [ counter, projectile.name ] 
@@ -28,11 +20,13 @@ func spawn_projectile(projectile: RigidBody2D):
 	add_child(new_projectile)
 
 func _on_timer_timeout() -> void:
-	var projectile: RigidBody2D = projectiles[0]
+	var projectile: RigidBody2D = projectiles[randi() % len(projectiles)]
 	spawn_projectile(projectile)
 
 func _enter_tree() -> void:
 	player = find_child("Player")
+	outline = find_child("Outline")
+	
 	projectiles = find_children("*-Projectile")
 	timer = find_child("ProjectileTimer")
 	timer.timeout.connect(_on_timer_timeout)
