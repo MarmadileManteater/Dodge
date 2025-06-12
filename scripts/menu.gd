7960be36225e75072b68814b0ed735587a0f3ba3
Emma <MarmadileManteater@proton.me>
2025-06-12 11:03:13 -0400

    Add mouse inputs for menu items

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,3 +6,8 @@ const Pointer = preload("res://scripts/pointer.gd")
 
 func _enter_tree() -> void:
 	pointer = find_child("Pointer")
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if (event is InputEventMouseButton):
+		Input.action_press("ui_accept")
--- scripts/pointer.gd
+++ scripts/pointer.gd
@@ -5,6 +5,13 @@ var original_position: Vector2
 @export var min_position = 0
 @export var max_position = 4
 
+enum MenuItem {
+	PLAY,
+	OPTIONS,
+	SCORES,
+	CREDITS
+}
+
 func _enter_tree() -> void:
 	original_position = position
 
@@ -17,3 +24,23 @@ func set_cursor_position(given_position):
 
 func set_cursor_position_relatively(given_position):
 	set_cursor_position(cursor_position + given_position)
+
+func _on_play_label_mouse_entered() -> void:
+	set_cursor_position(MenuItem.PLAY)
+
+func _on_options_label_mouse_entered() -> void:
+	set_cursor_position(MenuItem.OPTIONS)
+
+func _on_scores_label_mouse_entered() -> void:
+	set_cursor_position(MenuItem.SCORES)
+
+func _on_credits_label_mouse_entered() -> void:
+	set_cursor_position(MenuItem.CREDITS)
+
+func _on_gui_input(event: InputEvent) -> void:
+	# allow clicking menu items
+	if (event is InputEventMouseButton):
+		var event_action = InputEventAction.new()
+		event_action.action = "ui_accept"
+		event_action.pressed = event.pressed
+		Input.parse_input_event(event_action)
