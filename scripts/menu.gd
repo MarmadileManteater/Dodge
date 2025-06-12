3a09f5e1ef59b3c81330c3c87f9c6cfe80cffc67
Emma <MarmadileManteater@proton.me>
2025-06-12 14:57:40 -0400

    don't show pointing hand when main menu disabled

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,11 +5,14 @@ const Credits = preload("res://scripts/credits.gd")
 @export var pointer: Pointer
 var play_label: RichTextLabel
 var play_label_backdrop: RichTextLabel
+var options_label: RichTextLabel
+var scores_label: RichTextLabel
+var credits_label: RichTextLabel
 var audio_settings: TileMapLayer
 var music_slider: Sprite2D
 var effect_slider: Sprite2D
 var high_scores: Node2D
-var scores_label: RichTextLabel
+var dynamic_scores_label: RichTextLabel
 var scores_label_backdrop: RichTextLabel
 var credits: Credits
 
@@ -17,13 +20,22 @@ func _enter_tree() -> void:
 	pointer = find_child("Pointer")
 	play_label = find_child("PlayLabel")
 	play_label_backdrop = find_child("PlayLabel Back")
+	options_label = find_child("OptionsLabel")
+	scores_label = find_child("ScoresLabel")
+	credits_label = find_child("CreditsLabel")
 	audio_settings = find_child("Audio Settings")
 	music_slider = audio_settings.find_child("Music Slider")
 	effect_slider = audio_settings.find_child("Effect Slider")
 	high_scores = find_child("High Scores")
-	scores_label = high_scores.find_child("Scores Label")
+	dynamic_scores_label = high_scores.find_child("Scores Label")
 	scores_label_backdrop = high_scores.find_child("Scores Label Backdrop")
 	credits = find_child("Credits")
+	
+func set_default_cursor_shape(shape: Control.CursorShape):
+	play_label.mouse_default_cursor_shape = shape
+	options_label.mouse_default_cursor_shape = shape
+	scores_label.mouse_default_cursor_shape = shape
+	credits_label.mouse_default_cursor_shape = shape
 	
 func toggle_audio_settings():
 	audio_settings.visible = !audio_settings.visible
@@ -48,7 +60,7 @@ func set_first_option_text(text = "Play"):
 	play_label_backdrop.text = text
 
 func set_scores_text(text):
-	scores_label.text = text
+	dynamic_scores_label.text = text
 	scores_label_backdrop.text = text
 	
 func set_bgm_volume_slider(relative_position):
